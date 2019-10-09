8deacba7a298eeada49383d7d381ebcac16c2d76
efichot <fichotetienne@gmail.com>
2019-10-09 13:22:47 +0700

    first commit: scaffold project ✅

---
+++ lib/app.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+import 'login.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shrine',
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/login',
+      onGenerateRoute: _getRoute,
+    );
+  }
+
+  Route<dynamic> _getRoute(RouteSettings settings) {
+    if (settings.name != '/login') {
+      return null;
+    }
+
+    return MaterialPageRoute<void>(
+      settings: settings,
+      builder: (BuildContext context) => LoginPage(),
+      fullscreenDialog: true,
+    );
+  }
+}
