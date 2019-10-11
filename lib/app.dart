c7f030d68a9c586d52de43d9fb71143e47cb9a23
efichot <fichotetienne@gmail.com>
2019-10-11 12:12:09 +0700

    GridView and Card added ✅

--- lib/app.dart
+++ lib/app.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(fontFamily: 'Rubik'),
       home: HomePage(),
       debugShowCheckedModeBanner: false,
-      initialRoute: '/login',
+      initialRoute: '/',
       onGenerateRoute: _getRoute,
     );
   }
