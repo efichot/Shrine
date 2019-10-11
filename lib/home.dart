c82f2f8cf94213b803d4a1105bfe532eccbf3379
efichot <fichotetienne@gmail.com>
2019-10-11 11:39:31 +0700

    Adding leading and actions buttons to the appBar ✅

--- lib/home.dart
+++ lib/home.dart
@@ -4,6 +4,38 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            semanticLabel: 'menu',
+          ),
+          onPressed: () {
+            print('menu toggle');
+          },
+        ),
+        title: Text('SHRINE'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              semanticLabel: 'search',
+            ),
+            onPressed: () {
+              print('search');
+            },
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.tune,
+              semanticLabel: 'tune',
+            ),
+            onPressed: () {
+              print('tune');
+            },
+          )
+        ],
+      ),
       body: Center(
         child: Text('You did it!'),
       ),
--- lib/login.dart
+++ lib/login.dart
@@ -57,7 +57,7 @@ class _LoginPageState extends State<LoginPage> {
                 RaisedButton(
                   child: Text('NEXT'),
                   onPressed: () {
-                    Navigator.pushNamed(context, '/');
+                    Navigator.pop(context);
                   },
                 ),
               ],
