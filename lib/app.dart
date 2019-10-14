43947c6e0b51f6701c6e4f7eb0409489f186db74
efichot <fichotetienne@gmail.com>
2019-10-14 14:58:53 +0700

    Incorporation of the shrine theme ⚙️

--- lib/app.dart
+++ lib/app.dart
@@ -1,14 +1,58 @@
 import 'package:flutter/material.dart';
 
+import 'colors.dart';
 import 'home.dart';
 import 'login.dart';
+
+final ThemeData _kShrineTheme = _buildShrineTheme();
+
+ThemeData _buildShrineTheme() {
+  final ThemeData base = ThemeData(fontFamily: 'Rubik');
+
+  return base.copyWith(
+      brightness: Brightness.dark,
+      accentColor: kShrineBrown900,
+      primaryColor: kShrinePink100,
+      buttonTheme: base.buttonTheme.copyWith(
+        buttonColor: kShrinePink100,
+        textTheme: ButtonTextTheme.normal,
+      ),
+      scaffoldBackgroundColor: kShrineBackgroundWhite,
+      cardColor: kShrineBackgroundWhite,
+      textSelectionColor: kShrinePink100,
+      errorColor: kShrineErrorRed,
+      textTheme: _buildShrineTextTheme(base.textTheme),
+      primaryTextTheme: _buildShrineTextTheme(base.primaryTextTheme),
+      accentTextTheme: _buildShrineTextTheme(base.accentTextTheme),
+      primaryIconTheme: base.iconTheme.copyWith(color: kShrineBrown900),
+      inputDecorationTheme: InputDecorationTheme(border: OutlineInputBorder()));
+}
+
+TextTheme _buildShrineTextTheme(TextTheme base) {
+  return base
+      .copyWith(
+        headline: base.headline.copyWith(
+          fontWeight: FontWeight.w500,
+        ),
+        title: base.title.copyWith(fontSize: 18.0),
+        caption: base.caption.copyWith(
+          fontWeight: FontWeight.w400,
+          fontSize: 12.0,
+        ),
+        button: base.button.copyWith(fontWeight: FontWeight.bold),
+      )
+      .apply(
+        displayColor: kShrineBrown900,
+        bodyColor: kShrineBrown900,
+      );
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Shrine',
-      theme: ThemeData(fontFamily: 'Rubik'),
+      theme: _kShrineTheme,
       home: HomePage(),
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
